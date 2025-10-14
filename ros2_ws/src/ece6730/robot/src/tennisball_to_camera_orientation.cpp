d40f8dc4dd81339a7f3385175f1f71022fc4cda4
Christopher Santos <christopher.santos1@mail.weber.edu>
2025-10-13 19:24:02 -0600

    last commit for the day

--- ros2_ws/src/ece6730/robot/src/tennisball_to_camera_orientation.cpp
+++ ros2_ws/src/ece6730/robot/src/tennisball_to_camera_orientation.cpp
@@ -15,22 +15,35 @@ public:
         [this](message_interfaces::msg::TennisBallDetection::UniquePtr msg) -> void {
             RCLCPP_INFO(this->get_logger(), "I heard: 'left_x:%d /n right_x:%d /n top_y:%d /n bottom_y:%d'", msg->left_x, msg->right_x, msg->top_y, msg->bottom_y);
         };
+
+        auto distance_processing_callback =
+        [this](message_interfaces::msg::TennisBallDetection::UniquePtr msg) -> void {
+            float r = 
+            x = processX(*msg, r);
+            y = processY(*msg, r);
+            z = processZ(*msg, r);
+            RCLCPP_INFO(this->get_logger(), "x: %f, y: %f, z: %f", x, y, z);
+        };
+
         detection_info_subscription =
         this->create_subscription<message_interfaces::msg::TennisBallDetection>("detection_info", 5, topic_callback);
     }
 
 private:
     rclcpp::Subscription<message_interfaces::msg::TennisBallDetection>::SharedPtr detection_info_subscription;
-    private float camera_fov = 70.42; 
-
-    private float processX(){
+    private float camera_fov = 70.42;
+    private float tennis_ball_width =  .067; // meters
+    private float x;
+    private float y;
+    private float z;
+    private float processX(const message_interfaces::msg::TennisBallDetection::UniquePtr &msg, float r){
 
     }
-    private float processY(){
+    private float processY(const message_interfaces::msg::TennisBallDetection::UniquePtr &msg, float r){
 
     }
-    private float processZ(){
-    
+    private float processZ(const message_interfaces::msg::TennisBallDetection::UniquePtr &msg, float r){
+
     }
 };
 
